7b26d95fe74dbffd4f8903e57f116da3df86e931
mehdizebhi <mehdizebhi@yahoo.com>
2024-04-23 23:08:43 +0330

    update : add layout for left side & header and right side

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,52 +1,202 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+  <q-layout view="hHh lpR fFf">
+    <q-header class="bg-primary text-white">
+      <q-toolbar class="justify-between">
+        <div style="width: 380px;" class="row q-mr-sm sm-hide xs-hide">
+          <q-btn-dropdown dense flat rounded no-icon-animation dropdown-icon="menu">
+            <q-list>
+              <q-item clickable v-close-popup @click="onItemClick">
+                <q-item-section>
+                  <q-item-label>Photos</q-item-label>
+                </q-item-section>
+              </q-item>
 
-        <q-toolbar-title>
-          Chat App
-        </q-toolbar-title>
+              <q-item clickable v-close-popup @click="onItemClick">
+                <q-item-section>
+                  <q-item-label>Videos</q-item-label>
+                </q-item-section>
+              </q-item>
 
-        <div>Quasar v{{ $q.version }}</div>
+              <q-item clickable v-close-popup @click="onItemClick">
+                <q-item-section>
+                  <q-item-label>Articles</q-item-label>
+                </q-item-section>
+              </q-item>
+            </q-list>
+          </q-btn-dropdown>
+          <q-toolbar-title>
+            <q-input color="white" rounded outlined v-model="search" label="Search">
+              <template v-slot:append>
+                <q-icon v-if="search !== ''" name="close" @click="search = ''" class="cursor-pointer"/>
+                <q-icon v-else name="search"/>
+              </template>
+            </q-input>
+          </q-toolbar-title>
+        </div>
+        <div class="col row justify-between items-center">
+          <div class="col-3">
+            <q-item clickable v-ripple>
+            <q-item-section side>
+              <q-avatar size="48px">
+                <img src="https://cdn.quasar.dev/img/avatar.png">
+              </q-avatar>
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Mary</q-item-label>
+              <q-item-label caption>Last Seen Recently</q-item-label>
+            </q-item-section>
+          </q-item>
+          </div>
+          <div class="">
+            <q-btn dense flat round icon="notifications"/>
+            <q-btn dense flat round icon="search  "/>
+            <q-btn dense flat round icon="more_vert" @click="toggleRightDrawer"/>
+          </div>
+        </div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      bordered
-    >
+    <q-drawer width="400" v-model="leftDrawerOpen" side="left" bordered>
       <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
+        <q-item-label header>
         </q-item-label>
 
-        <EssentialLink
-          v-for="link in linksList"
-          :key="link.title"
-          v-bind="link"
-        />
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-avatar>
+              <img src="https://cdn.quasar.dev/img/avatar2.jpg">
+            </q-avatar>
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label lines="1">Brunch this weekend?</q-item-label>
+            <q-item-label caption lines="2">
+              <span class="text-weight-bold">Janet</span>
+              -- I'll be in your neighborhood doing errands this
+              weekend. Do you want to grab brunch?
+            </q-item-label>
+          </q-item-section>
+
+          <q-item-section side top>
+            1 min ago
+          </q-item-section>
+        </q-item>
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-avatar>
+              <img src="https://cdn.quasar.dev/img/avatar2.jpg">
+            </q-avatar>
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label lines="1">Brunch this weekend?</q-item-label>
+            <q-item-label caption lines="2">
+              <span class="text-weight-bold">Janet</span>
+              -- I'll be in your neighborhood doing errands this
+              weekend. Do you want to grab brunch?
+            </q-item-label>
+          </q-item-section>
+
+          <q-item-section side top>
+            1 min ago
+          </q-item-section>
+        </q-item>
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-avatar>
+              <img src="https://cdn.quasar.dev/img/avatar2.jpg">
+            </q-avatar>
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label lines="1">Brunch this weekend?</q-item-label>
+            <q-item-label caption lines="2">
+              <span class="text-weight-bold">Janet</span>
+              -- I'll be in your neighborhood doing errands this
+              weekend. Do you want to grab brunch?
+            </q-item-label>
+          </q-item-section>
+
+          <q-item-section side top>
+            1 min ago
+          </q-item-section>
+        </q-item>
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-avatar>
+              <img src="https://cdn.quasar.dev/img/avatar2.jpg">
+            </q-avatar>
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label lines="1">Brunch this weekend?</q-item-label>
+            <q-item-label caption lines="2">
+              <span class="text-weight-bold">Janet</span>
+              -- I'll be in your neighborhood doing errands this
+              weekend. Do you want to grab brunch?
+            </q-item-label>
+          </q-item-section>
+
+          <q-item-section side top>
+            1 min ago
+          </q-item-section>
+        </q-item>
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-avatar>
+              <img src="https://cdn.quasar.dev/img/avatar2.jpg">
+            </q-avatar>
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label lines="1">Brunch this weekend?</q-item-label>
+            <q-item-label caption lines="2">
+              <span class="text-weight-bold">Janet</span>
+              -- I'll be in your neighborhood doing errands this
+              weekend. Do you want to grab brunch?
+            </q-item-label>
+          </q-item-section>
+
+          <q-item-section side top>
+            1 min ago
+          </q-item-section>
+        </q-item>
+        <q-item clickable v-ripple>
+          <q-item-section avatar>
+            <q-avatar>
+              <img src="https://cdn.quasar.dev/img/avatar2.jpg">
+            </q-avatar>
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label lines="1">Brunch this weekend?</q-item-label>
+            <q-item-label caption lines="2">
+              <span class="text-weight-bold">Janet</span>
+              -- I'll be in your neighborhood doing errands this
+              weekend. Do you want to grab brunch?
+            </q-item-label>
+          </q-item-section>
+
+          <q-item-section side top>
+            1 min ago
+          </q-item-section>
+        </q-item>
+
       </q-list>
     </q-drawer>
 
+    <q-drawer v-model="rightDrawerOpen" side="right" bordered>
+      <!-- drawer content -->
+    </q-drawer>
+
     <q-page-container>
-      <router-view />
+      <router-view/>
     </q-page-container>
   </q-layout>
 </template>
 
 <script>
-import { defineComponent, ref } from 'vue'
-import EssentialLink from 'components/EssentialLink.vue'
+import {defineComponent, ref} from 'vue'
 
 const linksList = [
   {
@@ -60,20 +210,23 @@ const linksList = [
 export default defineComponent({
   name: 'MainLayout',
 
-  components: {
-    EssentialLink
-  },
+  components: {},
 
-  data () {
+  data() {
     return {
       linksList,
-      leftDrawerOpen: false
+      leftDrawerOpen: true,
+      rightDrawerOpen: false,
+      search: ""
     }
   },
 
   methods: {
-    toggleLeftDrawer () {
+    toggleLeftDrawer() {
       this.leftDrawerOpen = !this.leftDrawerOpen
+    },
+    toggleRightDrawer() {
+      this.rightDrawerOpen = !this.rightDrawerOpen
     }
   }
 })
