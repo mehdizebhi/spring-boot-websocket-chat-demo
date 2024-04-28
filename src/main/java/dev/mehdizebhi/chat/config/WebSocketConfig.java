be8103eda95ab4cebcce20da17f37f50622d0b46
mehdizebhi <mehdizebhi@yahoo.com>
2024-04-28 18:05:34 +0330

    update : config stomp message broker through websocket

---
+++ src/main/java/dev/mehdizebhi/chat/config/WebSocketConfig.java
@@ -0,0 +1,50 @@
+package dev.mehdizebhi.chat.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.DefaultContentTypeResolver;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.converter.MessageConverter;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    // 	/ws is the HTTP URL for the endpoint to which a WebSocket
+    // 	(or SockJS) client needs to connect for the WebSocket handshake.
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+
+        // STOMP messages whose destination header begins with /app are routed to @MessageMapping methods in @Controller classes.
+        registry.setApplicationDestinationPrefixes("/app");
+
+        registry.setUserDestinationPrefix("/user");
+
+        // 	Use the built-in message broker for subscriptions and broadcasting and route messages whose destination header begins with /user to the broker.
+        registry.enableSimpleBroker("/user");
+    }
+
+    @Override
+    public boolean configureMessageConverters(List<MessageConverter> messageConverters) {
+        DefaultContentTypeResolver resolver = new DefaultContentTypeResolver();
+        resolver.setDefaultMimeType(MimeTypeUtils.APPLICATION_JSON);
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setObjectMapper(new ObjectMapper());
+        converter.setContentTypeResolver(resolver);
+        messageConverters.add(converter);
+        return false;
+    }
+}
